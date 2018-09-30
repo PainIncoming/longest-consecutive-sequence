c761d530990b81d3e690a3ad8f2df3c8d25c0c31
PainIncoming <galko3.v@gmail.com>
2018-09-30 22:45:44 +0300

    Improve speed of execution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function longestConsecutiveLength(arr) {
-  let longest = { curr: 1, max: 1 },
-    length = arr.length;
+  let longest    = { curr: 1, max: 1 },
+      collection = new Set(arr);
 
-  arr.sort((a, b) => a - b);
+  collection.forEach(item => {
+    for (let next = item + 1; collection.has(next); next++) {
+        if (++longest.curr > longest.max) longest.max = longest.curr;
+    }
 
-  for (let i = 0; i < length; i++) {
-    if (arr[i] + 1 === arr[i + 1]) {
-      if (++longest.curr > longest.max) longest.max = longest.curr;
-    } else if (arr[i] !== arr[i + 1]) longest.curr = 1;
-  }
+    longest.curr = 1;
+  });
 
-  return length ? longest.max : 0;
+  return collection.size ? longest.max : 0;
 }
